fc361976ace59e3350182d3b8135a9b1707ef891
Victor Guzmán <80615275+GuzmanVic@users.noreply.github.com>
2023-10-04 16:19:45 -0600

    API implementada
    se terminó la implementaión de la API, falta organizar los pokemon por generaciones para evitar largos tiempos de carga

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
 const listaPokemon = document.querySelector("#listaPokemon");
+const botonesHeader = document.querySelectorAll(".btn-header");
 let URL = "https://pokeapi.co/api/v2/pokemon/";
+const limite=151;
 
-for (let i = 1; i <= 151; i++) {
-    fetch(URL + i)
-        .then(response => response.json())
-        .then(data => mostrarPokemon(data));
-}
 
+async function cargarPokemones() {
+    const requests = [];
+  
+    for (let i = 1; i <= 151; i++) {
+      requests.push(fetch(URL + i).then((response) => response.json()));
+    }
+  
+    const pokemones = await Promise.all(requests);
+    pokemones.forEach((data) => mostrarPokemon(data));
+  }
 function mostrarPokemon(poke) {
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    div.innerHTML = `
+  let tipos = poke.types.map(
+    (type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`
+  );
+  tipos = tipos.join("");
+
+  let pokeId = poke.id.toString();
+  if (pokeId.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId.length === 2) {
+    pokeId = "0" + pokeId;
+  }
+
+  const div = document.createElement("div");
+  div.classList.add("pokemon");
+  div.innerHTML = `
     <p class="pokemon-id-back">#${poke.id}</p>
     <div class="pokemon-imagen">
         <img src="${poke.sprites.other["official-artwork"].front_default}"
@@ -18,39 +37,43 @@ function mostrarPokemon(poke) {
     </div>
     <div class="pokemon-info">
         <div class="nombre-contenedor">
-            <p class="pokemon-id">#${poke.id}</p>
+            <p class="pokemon-id">#${pokeId}</p>
             <h2 class="pokemon-nombre">${poke.name}</h2>
         </div>
         <div class="pokemon-tipos">
-            <p class="electric tipo">Electric</p>
-            <p class="fighting tipo">Fight</p>
+       ${tipos}
         </div>
         <div class="pokemon-stats">
-            <p class="stat">4m</p>
-            <p class="stat">60kg</p>
+            <p class="stat">${poke.height}m</p>
+            <p class="stat">${poke.weight}kg</p>
         </div>
     </div>
     `;
-    listaPokemon.append(div);
+  listaPokemon.append(div);
 }
-// // {/* <div class="pokemon">
-// // <p class="pokemon-id-back">#025</p>
-// // <div class="pokemon-imagen">
-// //     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg"
-// //         alt="pikachu">
-// // </div>
-// // <div class="pokemon-info">
-// //     <div class="nombre-contenedor">
-// //         <p class="pokemon-id">#025</p>
-// //         <h2 class="pokemon-nombre">Pikachu</h2>
-// //     </div>
-// //     <div class="pokemon-tipos">
-// //         <p class="electric tipo">Electric</p>
-// //         <p class="fighting tipo">Fight</p>
-// //     </div>
-// //     <div class="pokemon-stats">
-// //         <p class="stat">4m</p>
-// //         <p class="stat">60kg</p>
-// //     </div>
-// // </div>
-// // </div> */}
+cargarPokemones();
+botonesHeader.forEach((boton) =>
+  boton.addEventListener("click", (event) => {
+    const botonId = event.currentTarget.id;
+    listaPokemon.innerHTML = "";
+    const requests = [];
+
+    for (let i = 1; i <= 151; i++) {
+      requests.push(fetch(URL + i).then((response) => response.json()));
+    }
+
+    Promise.all(requests).then((pokemones) => {
+      pokemones.forEach((data) => {
+        if (botonId === "ver-todos") {
+          mostrarPokemon(data);
+        } else {
+          const tipos = data.types.map((type) => type.type.name);
+          if (tipos.some((tipo) => tipo.includes(botonId))) {
+            mostrarPokemon(data);
+          }
+        }
+      });
+    });
+  })
+);
+
